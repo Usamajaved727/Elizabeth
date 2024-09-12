8c469c1ae82359654da59c0c74677083b1f3570f
Usama <usamajaved727@gmail.com>
2024-09-12 19:44:13 +0500

    Your commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
-    './src/**/*.liquid',
-    './src/**/*.js',
-    './src/**/*.vue',
+    "./layout/**/*.liquid",   // Include all .liquid files in the layout folder
+    "./snippets/**/*.liquid", // Include all .liquid files in the snippets folder
+    "./templates/**/*.liquid", // Include any other relevant liquid files in the templates folder
+    "./sections/**/*.liquid",  // Sections folder for dynamic sections
+    "./assets/**/*.js",        // Any JavaScript files in the assets folder
+    "./assets/**/*.css",        // Any css files in the assets folder
+    "./**/*.{html,js}"         // HTML and JS files at any level
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
