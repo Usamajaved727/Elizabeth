d2a7b8c53bc72775cd9f4eeb15fe0b3db783d6fa
Usama <usamajaved727@gmail.com>
2024-11-04 19:38:46 +0500

    today update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./layout/**/*.liquid",   // Include all .liquid files in the layout folder
-    "./snippets/**/*.liquid", // Include all .liquid files in the snippets folder
-    "./templates/**/*.liquid", // Include any other relevant liquid files in the templates folder
-    "./sections/**/*.liquid",  // Sections folder for dynamic sections
-    "./assets/**/*.js",        // Any JavaScript files in the assets folder
-    "./assets/**/*.css",        // Any css files in the assets folder
-    "./**/*.{html,js}"         // HTML and JS files at any level
+    './**/*.liquid',    // Shopify Liquid files
+    './src/**/*.{js,css}', // Specify only the necessary directories and file types
   ],
   theme: {
     extend: {},
